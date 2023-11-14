7cf81f13447699d41a07a98df218360bac8243f1
HerikH <erik.harutyunyan.03.03@mail.ru>
2023-11-14 12:57:12 +0400

    pageNotFound

--- src/pages/404/pageNotFound.jsx
+++ src/pages/404/pageNotFound.jsx
@@ -1,6 +1,15 @@
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 import "./style.scss";
 
-function pageNotFound() {
-  return <div>pageNotFound</div>;
-}
-export default pageNotFound;
+function PageNotFound() {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default PageNotFound;
